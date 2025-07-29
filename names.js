77fe49ca59c6c79a723014793312111f4c3baf9c
dpalexandru <alexpoian91@hotmail.com>
2025-07-29 15:36:58 +0200

    feat: create a function for full name object

--- names.js
+++ names.js
@@ -1,3 +1,14 @@
 //Crea una funzione che accetta due parametri: firstName, lastName. 
 //La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 //Esporta la funzione dal file.
+
+//Creo la funzione che restituisce un oggetto
+
+const nomeCompleto = (firstName, lastName) => {
+    return {
+        firstName,
+        lastName,
+    }
+};
+
+module.exports = nomeCompleto;
