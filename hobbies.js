6ca3c3b1a524d6d2c5183f45884f1422d8be251a
dpalexandru <alexpoian91@hotmail.com>
2025-07-29 15:41:46 +0200

    feat: create a function to return an hobbies array

---
+++ hobbies.js
@@ -0,0 +1,12 @@
+//Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+//Esporta la funzione dal file.
+
+// creo la funzione che restituisce un array con 3 hobby
+
+const hobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+    return [
+        hobbyOne, hobbyTwo, hobbyThree
+    ]
+};
+
+module.exports = hobbies;
